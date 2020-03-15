f96ee47f94603725b0e9b32f2c026a7b9cd2f109
IskraNikolova <air_iskra@yahoo.com>
2020-03-15 18:05:17 +0200

    Fix card element of Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,22 +2,27 @@
   <q-page class="q-pa-xl">
     <div class="row">
       <div class="col-3" v-for="i in images" v-bind:key="i">
-        <q-img :src="'./../statics/res/' + i + '.png'" native-context-menu @mouseover="hover(i)" @mouseleave="leave(i)">
-          <q-icon :class="'absolute all-pointer-events' + ' ' + i" size="22px" name="favorite_border" color="white" style="top: 8px; left: 8px;display:none;">
-            <q-tooltip>
-              Любими
-            </q-tooltip>
-          </q-icon>
-          <q-icon :class="'absolute all-pointer-events' + ' ' + i" size="22px" name="add_shopping_cart" color="white" style="top: 8px; right: 8px;display:none;">
-            <q-tooltip>
-              Поръчай
-            </q-tooltip>
-          </q-icon>
-          <div :class="'absolute-bottom text-subtitle1 text-center' + ' ' + i" style="display:none;">
-            Име на продукта
-          </div>
-        </q-img>
-        <div style="text: center;"> 56.00 lv.</div>
+        <q-card>
+          <q-img :src="'./../statics/res/' + i + '.png'" native-context-menu @mouseover="hover(i)" @mouseleave="leave(i)">
+            <q-icon :class="'absolute all-pointer-events' + ' ' + i" size="22px" name="favorite_border" color="white" style="top: 8px; left: 8px;display:none;">
+              <q-tooltip>
+                Любими
+              </q-tooltip>
+            </q-icon>
+            <q-icon :class="'absolute all-pointer-events' + ' ' + i" size="22px" name="add_shopping_cart" color="white" style="top: 8px; right: 8px;display:none;">
+              <q-tooltip>
+                Добави в кошницата
+              </q-tooltip>
+            </q-icon>
+            <div :class="'absolute-bottom text-subtitle1 text-center' + ' ' + i" style="display:none;">
+              Име на продукта
+            </div>
+          </q-img>
+          <q-card-section>
+            <div style="text: center;"> 56.00 lv.</div>
+            <q-rating size="xs" v-model="stars" :max="5" />
+          </q-card-section>
+        </q-card>
       </div>
     </div>
   </q-page>
@@ -28,6 +33,7 @@ export default {
   name: 'PageIndex',
   data () {
     return {
+      stars: 4,
       images: [6, 17, 21, 23, 33, 42, 46, 55, 62, 76, 81, 85, 86, 88, 89, 90, 91, 92, 93, 94, 95, 97, 98, 102, 104, 106, 107, 108,
         109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120]
     }
